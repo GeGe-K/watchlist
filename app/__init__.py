ccf90d06b1262fdf2e41b8cf7212e81d02a3b196
GeGe-K <gloriagivondo@gmail.com>
2018-11-15 12:47:17 +0300

    created a photos folder in the app/static folder.

--- app/__init__.py
+++ app/__init__.py
@@ -3,18 +3,23 @@ from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads iport UploadSet, configure_uploads, IMAGES
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+photo = UploadSet('photo',IMAGES)
 
 def create_app(config_name):
     app = Flask(__name__)
 
     # Creating the app congigurations
     app.config.from_object(config_options[config_name])
+
+    # Configure UploasSet
+    configure_uploads(app,photos)
 
     # Initializing flask extensions
     bootstrap.init_app(app)
