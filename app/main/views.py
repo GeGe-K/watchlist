5f2efd5510efeb25e9e5148c9f1223f25e46b5f4
GeGe-K <gloriagivondo@gmail.com>
2018-11-15 12:33:18 +0300

    created a function to login/out update user.

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,10 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,abort
 from . import main
 from ..request import get_movies,get_movie,search_movie
-from ..models import Review
-from .forms import ReviewForm
+from ..models import Review, User
+from .forms import ReviewForm, UpdateProfile
+from .. import db
+from flask_login import login_required
 # Review = reviews.Review
 
 # Views
@@ -31,6 +33,33 @@ def index():
             
         return render_template('index.html', title=title,popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie)
 
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+    return render_template("profile/profile.html", user = user)
+
+
+@main.route('/user/<uname>/update', methods=['GET', 'POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username=uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile', uname=user.username))
+
+    return render_template('profile/update.html', form=form)
 
 @main.route('/movie/<int:id>')
 def movie(id):
@@ -56,6 +85,7 @@ def search(movie_name):
     return render_template('search.html', movies = searched_movies)
 
 @main.route('/movie/reviews/new/<int:id>', methods = ['GET','POST'])
+@login_required
 def new_review(id):
     form = ReviewForm()
     movie = get_movie(id)
--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
-
+from flask_login import UserMixin
+from . import login_manager
 class Movie:
     '''
     Movie class to define Movie Objects
@@ -44,12 +45,16 @@ class Review:
         return response
 
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
     role_id  = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    pass_secure = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
+    # pass_secure = db.Column(db.String(255))
 
     @property
     def password(self):
@@ -57,11 +62,15 @@ class User(db.Model):
 
     @password.setter
     def password(self, password):
-        self.pass_secure = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
     def verify_password(self, password):
-        return check_password_hash(self.pass_secure,password)
-        
+        return check_password_hash(self.password_hash,password)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
     def __repr__(self):
         return f'User {self.username}'
 
